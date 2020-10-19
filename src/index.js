40025b7ca4344ffb395f6c5706680db20e2b612d
Oleg Zakladnyi <zakladniy@gmail.com>
2020-10-19 22:42:25 +0300

    feat: fix problem

--- src/index.js
+++ src/index.js
@@ -37,29 +37,29 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-    function decode(expr) {
-        let startPos = 0;
-        let decoded = "";
-        for (let i = 0; i < expr.length/10; i++) {
-          let letterNums = expr.slice(startPos, startPos + 10);
-          startPos += 10;
-          if (letterNums == "**********") {
-            decoded += " ";
-            continue;
-          }
-          let morsed = "";
-          for (let j = 10; j > 0; j -= 2) {
-            switch( letterNums.slice(j-2, j) ) {
-              case "10" : morsed += "."; break;
-              case "11" : morsed += "-"; break; 
-              default: break;
-            }
-          }
-          decoded += MORSE_TABLE[ morsed.split("").reverse().join("") ];
-        }
-        return decoded;
+function decode(expr) {
+    let startPos = 0;
+    let decoded = "";
+    for (let i = 0; i < expr.length/10; i++) {
+      let letterNums = expr.slice(startPos, startPos + 10);
+      startPos += 10;
+      if (letterNums == "**********") {
+        decoded += " ";
+        continue;
       }
+      let morsed = "";
+      for (let j = 10; j > 0; j -= 2) {
+        switch( letterNums.slice(j-2, j) ) {
+          case "10" : morsed += "."; break;
+          case "11" : morsed += "-"; break; 
+          default: break;
+        }
+      }
+      decoded += MORSE_TABLE[ morsed.split("").reverse().join("") ];
+    }
+    return decoded;
 }
+
 
 module.exports = {
     decode
